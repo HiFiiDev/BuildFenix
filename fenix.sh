ea31e6bd3670019f5ac680582566913d9771861d
hifii <mbqsniper@gmail.com>
2019-10-18 09:57:56 +0000

    fixes

--- fenix.sh
+++ fenix.sh
@@ -3,7 +3,7 @@
 
 # You just need to adjust the hardcoded values
 # in the script for it to work if youd like to use it.
-# Make sure to have fenix cloned and point it fo the right
+# Make sure to have fenixcloned and point it fo the right
 # dirextory. same thing for anything related to my server
 # directory I hardcoded for now
 
@@ -17,6 +17,12 @@ GREEN=$'\e[0;32m'
 On_Blu='\e[44m';
 # bold white
 BWhi='\e[1;37m';
+var=" Number "
+var+=$((var + 2))
+varAlt=$((var + 2))
+doubleUnderscore="__"
+fenixHomeCountUp=/var/www/html/Fenix/Builds/$TodaysDate$var
+fenixHomeCountUpUnderscore=/var/www/html/Fenix/Builds/$TodaysDate$doubleUnderscore$varAlt
 
 # Alias for echo to handle escape codes like colors
 function echo() {
@@ -27,12 +33,17 @@ function echo() {
 while [ $# -gt 0 ]
 do
     case $1 in
-        '--debug')
+        '--opti')
             clear
             for l in It worked ; do
-                echo -n ${bold}$l
-                echo " "
-                eecho " "
+                if [ ! -f "build.sh" ] ; then
+                    echo "Downloading file... "
+                    wget http://get.hifiibuilds.com/Scripts/opti.sh
+                    echo " "
+                    sleep 0.5
+                    echo "Done"
+                    chmod a+x opti.sh && sh opti.sh
+                fi
             done
             ;;
         *)
@@ -44,39 +55,45 @@ done
 
 buildFenix() {
     cd $Fenix
+
+    echo "updating source..."
+    echo " "
+    sleep 0.5
+
     git pull
 
     # WIP #
     if [ ! -f "build.sh" ] ; then
+        echo "Downloading build script..."
         wget http://get.hifiibuilds.com/Scripts/build.sh
         chmod a+x build.sh
     fi
 
     echo " "
     echo " "
-    echo "Building. Might take a few moments."
+    echo  "Building. \nThis will take a few moments."
     sleep 0.5
-    echo " "
-    echo " "
 
     # run this but thread it so it gets done faster
     #    for d in /$Fenix ; do (cd "$d" && find -name '*.sh' -print0 | xargs -P8 -L1 -0 sh build.sh); done
-    sh build.sh
+    ./build.sh
     echo " "
     echo " "
     echo "Should be done now."
     sleep 0.2
     echo " "
-    echo "Onto signing app..."
 }
 
 signFenix() {
-    java -jar ~/signer.jar -a ~/fenix/app/build/outputs/apk/geckoNightly/fenixNightly/app-geckoNightly-arm64-v8a-fenixNightly-unsigned.apk --out $FenixHome
+    echo "Signing apk... "
+    #   echo -e "\e[1;97m **TEXT OR COMMAND HERE**
+    #    java -jar ~/signer.jar -a ~/fenix/app/build/outputs/apk/geckoNightly/fenixNightly/app-geckoNightly-arm64-v8a-fenixNightly-unsigned.apk --out $FenixHomeCountUp
+    java -jar ~/signer.jar -a ~/fenix/app/build/outputs/apk/geckoNightly/fenixNightly/app-geckoNightly-arm64-v8a-fenixNightly-unsigned.apk --out $fenixHomeCountUpUnderscore
 }
 
 doCd() {
     if [ ! -d $FenixHome ]; then mkdir -p $FenixHome; fi
-    #  if [ -d "$FenixHome/$TodaysDate" ]; then cd $FenixHome && mkdir -p $TodaysDate__2; fi
+    if [ -d "$FenixHome/$TodaysDate" ]; then cd $FenixHome && mkdir -p $TodaysDate+$var && echo "made dir!"; fi
 
     #if [ -d "$FenixHome/$TodaysDate" ]; then
     # cd $FenixHome && mkdir -p $TodaysDate__2
@@ -85,20 +102,6 @@ doCd() {
 makeChangelog() {
     cd $FenixChanges
     if [ ! -d $FenixChanges/$TodaysDate ]; then mkdir -p $FenixChanges/$TodaysDate; fi
-
-    cd $Fenix
-    git log --reverse --no-merges --since=1.day.ago >> Fenix_changelog_$TodaysDate.txt
-    mv Fenix_changelog_$TodaysDate.txt $FenixChanges/$TodaysDate/
-}
-
-buildFenix
-doCd
-signFenix
-makeChangelog
-cd $FenixHome
-mv app-geckoNightly-arm64-v8a-fenixNightly-aligned-debugSigned.apk Fenix-Arm64-hifii__nightly-$(date +"%m-%d-%Y").apk
-if [ -d "$FenixHome/$TodaysDate" ]; then mkdir -p 10-06-2019__2 ; fi
-d $FenixChanges/$TodaysDate ]; then mkdir -p $FenixChanges/$TodaysDate; fi
 
     cd $Fenix
     git log --reverse --no-merges --since=1.day.ago >> Fenix_changelog_$TodaysDate.txt
@@ -114,7 +117,8 @@ sleep 1
 buildFenix
 doCd
 signFenix
+if [ -d "$FenixHome/$TodaysDate" ]; then mkdir -p $fenixHomeCountUp ; fi
 makeChangelog
 cd $FenixHome
-mv app-geckoNightly-arm64-v8a-fenixNightly-aligned-debugSigned.apk Fenix-Arm64-hifii__nightly-$(date +"%m-%d-%Y")$var.apk
-if [ -d "$FenixHome/$TodaysDate" ]; then mkdir -p $fenixHomeCountUp ; fi
+# mv app-geckoNightly-arm64-v8a-fenixNightly-aligned-debugSigned.apk Fenix-Arm64-hifii__nightly-$(date +"%m-%d-%Y")$var.apk
+#if [ -d "$FenixHome/$TodaysDate" ]; then mkdir -p $fenixHomeCountUp ; fi
